2945f092a6461507c7dca53b7a4b12b34f01a220
Stijn Debrouwere <stijn@stdout.be>
2014-07-16 13:53:32 +0200

    Get CLI working with docopt.

--- antenna/__init__.py
+++ antenna/__init__.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
 # encoding: utf-8
 
-""" Antenna.
+"""Antenna.
 
 Usage:
-    antennna listen <profile> <queue> <command>
+    antenna listen <profile> <queue> <command>
     antenna configure <profile> <queue> <command>
 
 The most stupidly simple way to scale an application is through a message queue,
@@ -26,6 +25,10 @@ AWS CLI credentials live) or `~/.boto`.
     aws_secret_access_key=<secret>
     region=<region>
 
+An easy way to see if everything is working would be to manually add a couple of 
+messages with the [SQS Management Console](https://console.aws.amazon.com/sqs/home), 
+and use `"cat >> log.txt"` as a command.
+
 Install Antenna with `pip`, the Python package installer.
 
     pip install sqs-antenna
@@ -44,7 +47,7 @@ If you're on Ubuntu, you can run Antenna as a daemon by adding it to Upstart:
 from docopt import docopt
 
 
-def listen(profile, queue_name, command):
+def listen(profile, queue, command):
     import sys
     import os
     import subprocess
@@ -55,6 +58,7 @@ def listen(profile, queue_name, command):
 
     # while Boto can read its own configuration just fine, we want Antenna 
     # to also work with AWS CLI configuration file
+    profile = 'profile ' + profile
     config = ConfigParser.ConfigParser()
     config.read(map(os.path.expanduser, ['~/.aws/config', '~/.boto']))
     region = config.get(profile, 'region')
@@ -65,7 +69,7 @@ def listen(profile, queue_name, command):
         aws_access_key_id=access_key, 
         aws_secret_access_key=secret_key)
 
-    queue = sqs.get_queue(queue_name)
+    queue = sqs.get_queue(queue)
 
     while True:
         messages = queue.get_messages(1, wait_time_seconds=20)
@@ -77,20 +81,35 @@ def listen(profile, queue_name, command):
             stdout, stderr = process.communicate(body)
 
             if stdout:
-                sys.stdout.writeline(stdout)
+                sys.stdout.write(stdout)
 
             if stderr:
-                sys.stderr.writeline(stderr)
+                sys.stderr.write(stderr)
             else:
                 queue.delete_message(message)
 
 
+def extract(d, whitelist=None):
+    keys = [key.strip('<>') for key in d.keys()]
+    d = dict(zip(keys, d.values()))
+
+    if not whitelist:
+        whitelist = keys
+
+    return {k: v for k, v in d.items() if k in whitelist}
+
 
 def main():
     arguments = docopt(__doc__, version='Antenna 0.1')
+    kwargs = extract(arguments, ['profile', 'queue', 'command'])
 
-    # TODO: Upstart configuration template
-    listen(**kwargs)
+    if 'listen' in arguments:
+        listen(**kwargs)
+    elif 'configure' in arguments:
+        # TODO: Upstart configuration template
+        del arguments['configure']
+        raise NotImplementedError()
+
 
 if __name__ == '__main__':
     main()
